6a25e78ca9c7e4bc18df49ee279d2381bcdc205b
Georgi Delchev <g.delchev93@gmail.com>
2019-02-24 11:47:14 +0200

    optimized imports for test file

--- src/test/java/PairFinderTest.java
+++ src/test/java/PairFinderTest.java
@@ -1,14 +1,9 @@
-import com.sun.org.glassfish.gmbal.Description;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashMap;
 import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 class PairFinderTest {
 
